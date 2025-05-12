df55e3424e278e7914867514ed8b53f961e2c2a6
LeoCofre <leocofre1981@gmail.com>
2025-05-12 00:44:18 -0400

    Controlador pedido

---
+++ controlador/ControladorPedido.py
@@ -0,0 +1,12 @@
+def agregar_pedido_db():
+    pass
+
+def buscar_pedido_db():
+    pass
+
+def eliminar_pedido_db():
+    pass
+
+def editar_pedido_db():
+    pass
+
---
+++ controlador/ControladorVenta.py
@@ -0,0 +1,11 @@
+def agregar_venta_db():
+    pass
+
+def buscar_venta_db():
+    pass
+
+def eliminar_venta_db():
+    pass
+
+def editar_venta_db():
+    pass

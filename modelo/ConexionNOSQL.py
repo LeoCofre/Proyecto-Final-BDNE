d8a784558451c0e2df62600c76fcc40a42e28c5e
LeoCofre <leocofre1981@gmail.com>
2025-05-13 15:02:51 -0400

    Vista pedido

--- modelo/ConexionNOSQL.py
+++ modelo/ConexionNOSQL.py
@@ -2,18 +2,17 @@ import pymongo
 from pymongo.errors import ConnectionFailure
 from pymongo import MongoClient
  
-def conectar_mongodb():
+import pymongo
+from pymongo.errors import ConnectionFailure
+
+def conectar_mongodb(): 
     try:
-        # Parámetros de conexión
         MONGO_URI = "mongodb://localhost:27017/"
         MONGO_BASE_DATOS = "botilleria"
 
-        # Establecer conexión
         cliente = pymongo.MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
         db = cliente[MONGO_BASE_DATOS]
-        
-        # Probar conexión
-        cliente.server_info()  # Esto lanza una excepción si la conexión falla
+        cliente.server_info()
         print(f"Conexión exitosa a {MONGO_BASE_DATOS}")
         return db
 

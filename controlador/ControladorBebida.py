6c5a161d3e5636d7d86cbf5d8d21adebd8650dd2
LeoCofre <leocofre1981@gmail.com>
2025-05-12 21:47:10 -0400

    Prueba

--- controlador/ControladorBebida.py
+++ controlador/ControladorBebida.py
@@ -5,4 +5,16 @@ def buscar_bebida_db(nombre_bebida):
 def editar_bebida_db(bebida):
     pass    
 def eliminar_bebida_db(bebida):
-    pass
+        conn=conectar()
+        try:
+            if conn is not None:
+                cursor=conn.cursor()
+                cursor.execute("DELETE FROM bebidas WHERE id_bebida=%s",
+                            (bebida.get_id_bebida(),))
+                conn.commit()
+                print("Bebida eliminada")
+        except Exception as e:
+            print(f"No se eliminaron registros {e}")
+        finally:
+            cursor.close()
+            conn.close()

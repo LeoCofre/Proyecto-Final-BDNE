997e27e3a57c6bbfd6b62f97e41910da565dec67
susana0994 <susypao09@gmail>
2025-05-12 23:26:21 -0400

    ...

--- controlador/ControladorCliente.py
+++ controlador/ControladorCliente.py
@@ -7,7 +7,8 @@ def agregar_cliente_db(cliente):
         if conn is not None:
            cursor= conn.cursor()
            cursor.execute("INSERT INTO clientes(nombres, apellidos, rut, fecha_nacimiento, direccion, telefono, correo) VALUES (%s,%s,%s,%s,%s,%s,%s)",
-           (cliente.get_nombres(), cliente.get_apellidos(), cliente.get_rut(), cliente.get_fecha_nacimiento(), cliente.get_direccion(), cliente.get_telefono(), cliente.get_correo()) )
+           (cliente.get_nombres(), cliente.get_apellidos(), cliente.get_rut(), cliente.get_fecha_nacimiento(), cliente.get_direccion(), cliente.get_telefono(), 
+            cliente.get_correo()) )
            conn.commit()
            print("Cliente ingresado con exito")
     except Exception as e:
@@ -46,7 +47,8 @@ def editar_cliente_db(cliente):
         if conn is not None:
             cursor = conn.cursor()
             cursor.execute("UPDATE clientes SET nombres=%s, apellidos=%s,rut=%s, fecha_nacimiento=%s, direccion=%s, telefono=%s, correo=%s WHERE id=%s",
-                           (cliente.get_nombres(), cliente.get_apellidos(), cliente.get_rut(), cliente.get_fecha_nacimiento(),cliente.get_direccion(),cliente.get_telefono(),cliente.get_correo(),cliente.get_id_cliente()))
+                           (cliente.get_nombres(), cliente.get_apellidos(), cliente.get_rut(), cliente.get_fecha_nacimiento(),cliente.get_direccion(),
+                            cliente.get_telefono(),cliente.get_correo(),cliente.get_id_cliente()))
             conn.commit()
             print("Cliente editado con exito")
     except Exception as e:
